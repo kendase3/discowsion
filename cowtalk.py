dbf043b1e0decc51e57b366e3f0a7f8cc1d4674b
kendase3 <ira@wereware.com>
2014-09-04 11:50:51 -0400

    first cowtalk commit

---
+++ cowtalk.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+
+from twisted.internet.protocol import Factory
+from twisted.protocols.basic import LineReceiver
+from twisted.internet import reactor
+import subprocess
+
+class CowtalkProtocol(LineReceiver):
+    def __init__(self, users):
+        self.users = users
+        self.name = None
+        self.state = "GETNAME"
+
+    def connectionMade(self):
+        self.sendLine("What's your name?")
+
+    def connectionLost(self, reason):
+        self.sendLine("Goodbye!")
+        if self.users.has_key(self.name):
+            del self.users[self.name]
+
+    def dataReceived(self, line):
+        # we don't want to evaluate the newline
+        line = line.rstrip()
+        # in either case, we want to allow quitting:
+        if line == 'q':
+            self.transport.loseConnection() 
+            return
+        # we handle data differently depending on our state
+        if self.state == "GETNAME":
+            self.handle_GETNAME(line)
+        else:
+            self.handle_CHAT(line)
+
+    def handle_GETNAME(self, name):
+        if self.users.has_key(name):
+            self.sendLine("Name taken, please choose another.")
+            return
+        self.name = name
+        self.users[name] = self
+        self.state = "CHAT"
+        self.sendLine("Welcome, %s!" % name)
+
+    def handle_CHAT(self, message):
+        message = self.cowtalkify(message)
+        message = "%s>\n%s" % (self.name, message)
+        for name, protocol in self.users.iteritems():
+            if protocol != self:
+                protocol.sendLine(message)
+                #for line in message:
+                #    protocol.sendLine(line)
+
+    def cowtalkify(self, message):
+        # note: returns multiple lines
+        outstr = subprocess.Popen(
+                ["cowsay", message], stdout=subprocess.PIPE).communicate()
+        return outstr[0]
+
+
+class CowtalkFactory(Factory):
+    protocol = CowtalkProtocol
+    def __init__(self, greeting='Welcome!'):
+        self.greeting = greeting
+        self.users = {} 
+    def buildProtocol(self, addr):
+        return CowtalkProtocol(self.users)
+
+reactor.listenTCP(8000, CowtalkFactory())
+reactor.run()
